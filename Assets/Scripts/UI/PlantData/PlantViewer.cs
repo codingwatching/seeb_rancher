8ca0e1a28b2a5ecee16e1277ca5f323c4f5415ef
Dan Miller <dsmiller95@gmail.com>
2020-12-29 23:21:59 -0600

    added fancy in-ui viewer of the currently selected plant

---
+++ Assets/Scripts/UI/PlantData/PlantViewer.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts.Plants;
+using Assets.Scripts.Utilities.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.PlantData
+{
+    public class PlantViewer: MonoBehaviour
+    {
+        public GameObjectVariable selectedPlant;
+
+
+        private void Awake()
+        {
+            selectedPlant.Value
+                .TakeUntilDestroy(this)
+                .Subscribe(newObject =>
+                {
+                    ClearPlantViewer();
+                    var plantContainer = newObject?.GetComponentInParent<PlantContainer>();
+                    if (plantContainer?.plantType != null)
+                    {
+                        SetupPlantViewer(plantContainer);
+                    }
+                }).AddTo(this);
+        }
+
+        private void SetupPlantViewer(PlantContainer plantContainer)
+        {
+            foreach(Transform transform in plantContainer.plantsParent.transform)
+            {
+                Instantiate(transform.gameObject, this.transform);
+            }
+        }
+
+        private void ClearPlantViewer()
+        {
+            gameObject.DestroyAllChildren();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PlantData/Turntable.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Plants;
+using Assets.Scripts.Utilities.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.PlantData
+{
+    public class Turntable: MonoBehaviour
+    {
+        public float rotationSpeed;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
