c2cbdcc67e5598cade3c81cbb6d2c8d8c2201006
Dan Miller <dsmiller95@gmail.com>
2021-08-18 18:19:53 -0500

    unify

--- Assets/Scripts/DOTSDataBinders/HealthToFloatVariableBinder.cs
+++ Assets/Scripts/DOTSDataBinders/HealthToFloatVariableBinder.cs
@@ -6,14 +6,29 @@ using UnityEngine;
 
 namespace Assets.Scripts.DOTSDataBinders
 {
+    [RequireComponent(typeof(HealthComponent))]
     public class HealthToFloatVariableBinder : MonoBehaviour, IConvertGameObjectToEntity
     {
         public FloatVariable target;
-        public HealthAuthoring healthEntityTarget;
 
         public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
         {
-            dstManager.AddComponentObject(entity, target);
+            dstManager.AddComponentObject(entity, this);
+        }
+    }
+
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    public class HealthToFloatVariableBindingSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities
+                .ForEach((
+                in HealthToFloatVariableBinder binding,
+                in HealthComponent health) =>
+                {
+                    binding.target.SetValue(health.currentHealth);
+                }).WithoutBurst().Run();
         }
     }
 }
