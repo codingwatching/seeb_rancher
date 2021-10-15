4bf2c43c06904debe29b303c3a31e4dbf57fcebe
Dan Miller <dsmiller95@gmail.com>
2021-10-15 14:55:09 -0500

    save the time of day too

---
+++ Assets/Scripts/Simulation/TimeOfDaySaver.cs
@@ -0,0 +1,40 @@
+﻿using Dman.LSystem.SystemRuntime.VolumetricData;
+using Dman.LSystem.SystemRuntime.VolumetricData.Layers;
+using Dman.LSystem.SystemRuntime.VolumetricData.NativeVoxels;
+using Dman.SceneSaveSystem;
+using Environment;
+using Simulation.VoxelLayers;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace Simulation
+{
+    [RequireComponent(typeof(Animator))]
+    public class TimeOfDaySaver : MonoBehaviour, ISaveableData
+    {
+        public string SunlightStateName;
+
+        public string UniqueSaveIdentifier => "TimeOfDay";
+
+        private Animator animator => GetComponent<Animator>();
+
+        public object GetSaveObject()
+        {
+            var currentTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            return currentTime;
+        }
+
+        public void SetupFromSaveObject(object save)
+        {
+            var time = (float)save;
+            animator.Play(SunlightStateName, 0, time);
+        }
+        public ISaveableData[] GetDependencies()
+        {
+            return new ISaveableData[0];
+        }
+    }
+}
